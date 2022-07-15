d4f34184de2a89e16afd38ca17610cb754a3350d
4iLeRmax <102809824+4iLeRmax@users.noreply.github.com>
2022-07-15 09:25:30 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ let menu = document.querySelector('.block__header');
 function slideMenu(){
   menu.classList.toggle('active');
 }
+
+let comments = document.querySelector('.comments__twitter');
+function openComment(){
+  comments.classList.toggle('active');
+}
