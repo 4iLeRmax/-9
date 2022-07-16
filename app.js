3322787869955c953db95807c60898cb7a50d7fe
4iLeRmax <102809824+4iLeRmax@users.noreply.github.com>
2022-07-16 18:35:51 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -7,3 +7,26 @@ let comments = document.querySelector('.comments__twitter');
 function openComment(){
   comments.classList.toggle('active');
 }
+
+const tabsBtn = document.querySelectorAll('.tabs_nav_btn');
+const tabsItems = document.querySelectorAll('.tabs_item');
+// console.log(tabsBtn);
+
+tabsBtn.forEach(function(item) {
+  item.addEventListener('click', function() {
+    let currentBtn = item;
+    let tabId = currentBtn.getAttribute('data-tab');
+    let currentTab = document.querySelector(tabId);
+    // console.log(currentTab);
+
+    
+    tabsBtn.forEach(function(item) {
+      item.classList.remove('active');
+    });
+    tabsItems.forEach(function(item) {
+      item.classList.remove('active');
+    });
+    currentBtn.classList.add('active');
+    currentTab.classList.add('active');
+  });
+});
